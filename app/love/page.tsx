ed8af2b064413ae034b7cfd060b394086939fd51
kashiki123 <joemariobrial54@gmail.com>
2024-11-11 20:43:11 -1200

    update 5.0

---
+++ app/love/page.tsx
@@ -0,0 +1,12 @@
+import { ValentineLoveCalculatorComponent } from "@/valentines/valentine-love-calculator";
+import React from "react";
+
+function Love() {
+  return (
+    <>
+      <ValentineLoveCalculatorComponent></ValentineLoveCalculatorComponent>
+    </>
+  );
+}
+
+export default Love;
